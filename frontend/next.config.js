a1809ab59152f2274c85e973c82e662223d2fe56
mani0408x <maniupgkp@gmail.com>
2025-12-29 22:20:38 +0530

    comit-message

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,19 +1,6 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ["@repo/ui"],
-    webpack: (config) => {
-        config.resolve.alias["react"] = path.dirname(require.resolve("react/package.json"));
-        config.resolve.alias["react-dom"] = path.dirname(require.resolve("react-dom/package.json"));
-        return config;
-    },
 };
 
 export default nextConfig;
